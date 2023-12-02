e01f8c827afb271ba197dcc2ff77cfa4061ff00a
Ramish Afaq <ramish881@gmail.com>
2023-12-02 12:24:41 +0500

    complete front end work

--- app/src/main/java/com/project/myapplication/Plans.java
+++ app/src/main/java/com/project/myapplication/Plans.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class creategroup3 extends AppCompatActivity {
+public class Plans extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_creategroup3);
+        setContentView(R.layout.activity_plans);
     }
 }
---
+++ app/src/main/java/com/project/myapplication/groupDetailsMember.java
@@ -0,0 +1,14 @@
+package com.project.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class groupDetailsMember extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_details_member);
+    }
+}
